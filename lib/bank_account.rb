eb0f6e79d585601a34defbc1e1b49bf9b972f256
Joseph Parker <jparker010190@gmail.com>
2018-05-20 15:28:04 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++
@@ -1,8 +0,0 @@
-class BankAccount
-  attr_accessor :name, :balance, :status
-  def initialize(name, balance=1000, status="open")
-    @name = name
-    @balance = balance
-    @status = status
-  end
-end
