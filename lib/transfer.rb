740b1a4a2968ede95ad6dedd7f2f8d8af16a6e77
Joseph Parker <jparker010190@gmail.com>
2018-05-20 15:34:39 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,4 +19,12 @@ class Transfer
       @status = 'rejected'
       'Transaction rejected. Please check your account balance.'
   end
+
+  def reverse_transfer
+    if @status == 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = 'reversed'
+    end
+  end
 end
